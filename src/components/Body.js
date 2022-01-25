79e724a8b9a5c30bb32f44b04b95ee6818413b48
vthanhnguyen <vthanhnguyen95@yahoo.com>
2022-01-24 19:00:11 -0800

    styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,11 @@
 import React from 'react'; //we need this for JSX
+import "../style.css"
 
 function Body(){
     return(
-        <div>
-            <p>Created by: Thanh, Umang, Christiana, Kevin</p>
+        <div className='body'>
             <ul>
-                <li>Humidity:</li>
+                <li>Humidity: </li>
                 <li>Temperature:</li>
                 <li>Sunlight:</li>
             </ul>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,11 @@
 import React from 'react'; //we need this for JSX
+import "../style.css"
 
 function Title(){
     return(
-        <div>
+        <div className="title">
             <h1>IoT: Automated Irrigation System</h1>
+            <p>Created by: Thanh, Umang, Christiana, Kevin</p>
         </div>
     )
 }
